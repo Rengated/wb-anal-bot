1efd89e9bbedf9254000d24d24ef54246604878f
rengated <kazievserg@gmail.com>
2024-07-01 22:35:38 +0300

    need only link

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,13 @@ const browser = await puppeteer.launch({
 
 bot.on("text", async (message: any) => {
   if (message.text == "/start") {
-    await bot.sendMessage(message.chat.id, "Привет, это бот для аналитики товаров, чтобы получить ответ, используй команду /article [ваш артикул]");
+    await bot.sendMessage(message.chat.id, "Привет, это бот для аналитики товаров, чтобы получить ответ, используй команду /link [ссылка на ваш товар]");
   }
-  if (message.text.startsWith("/article")) {
-    const [, aritcle] = message.text.split(" ");
-    if (aritcle) {
+  if (message.text.startsWith("/link")) {
+    const [, link] = message.text.split(" ");
+    if (link.includes("https://www.wildberries.ru/")) {
       try {
-        const feedbackId = await scrapHtml(aritcle, browser);
+        const feedbackId = await scrapHtml(link, browser);
         const feedbacks = await fetchFeedbacks(feedbackId);
         const filteredFeedbacks = await filterFeedbacks(feedbacks);
         const gptResponse = await getResponse(filteredFeedbacks.join("\n\n"));
@@ -34,6 +34,8 @@ bot.on("text", async (message: any) => {
         console.log(err);
         await bot.sendMessage(message.chat.id, "Произошла ошибка, или отзывы не найдены");
       }
+    } else {
+      await bot.sendMessage(message.chat.id, "Пришлите валидную ссылку на товар");
     }
   }
 });
--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,8 +1,9 @@
+import { link } from "fs";
 import puppeteer, { Browser } from "puppeteer";
 
-export const scrapHtml = async (aritcle: number, browser: Browser): Promise<string> => {
+export const scrapHtml = async (link: string, browser: Browser): Promise<string> => {
   const page = await browser.newPage();
-  await page.goto(`https://www.wildberries.ru/catalog/${aritcle}/detail.aspx`, { waitUntil: "networkidle2" });
+  await page.goto(link, { waitUntil: "networkidle2" });
 
   const linkElement = await page.$("a.product-page__reviews");
   const id = (await linkElement!.evaluateHandle((element) => (element as HTMLAnchorElement).href)).toString().split("?")[1].split("=")[1];
