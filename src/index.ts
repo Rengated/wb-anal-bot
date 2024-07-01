47d942efd5a22ccf2da6b5dd634b2a5f2f20d464
rengated <kazievserg@gmail.com>
2024-07-01 21:48:28 +0300

    change

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const bot = new TelegramBot(process.env.BOT_KEY!, {
   polling: true,
 });
 
-const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"], executablePath: "/usr/bin/chromium-browser" });
+const browser = await puppeteer.launch({ headless: false, args: ["--no-sandbox"], executablePath: "/usr/bin/chromium-browser" });
 
 bot.on("text", async (message: any) => {
   if (message.text == "/start") {
